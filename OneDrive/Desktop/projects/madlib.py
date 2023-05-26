835bbb7abc3e392f2402b9f3d6d4a2a420846e82
Rishit <rishitgrover4@gmail.com>
2023-05-25 20:34:13 -0600

    Madlib_Game

---
+++ OneDrive/Desktop/projects/madlib.py
@@ -0,0 +1,8 @@
+# madlib is a fun python program which focuses on filling up different blank spaces using the powers of 
+# string concatenation. To make this more fun the user has to choose the right word which will fill the paragraph 
+# in four tries. If the user is not able to fill the paragraph with the right word the user has to play the game again.
+Adjective = input("Adjective: ")
+verb = input("verb: ")
+verb_1 = input("verb_2: ")
+name = input("Name of the person: ")
+madlib_string = f"_________ is a(n) __________ activity that allows __________ to create __________ and __________ stories by filling in the blanks with various parts of speech such as __________, __________, __________, and __________. With the help of your Mad Lib program, players can easily generate customized stories by inputting their preferred words into designated blank spaces. This program provides an entertaining way to practice __________ and __________ skills while also unleashing __________ and __________. Whether it's a story about a __________ __________"
